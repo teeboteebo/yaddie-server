7583306f8a49afed4459589e71c36c9fed37dc54
Jesper Asplund <jesper.asplund95@gmail.com>
2019-09-16 15:40:35 +0200

    updated ingredient schema model

--- schemas/Ingredient.js
+++ schemas/Ingredient.js
@@ -4,14 +4,7 @@ const Schema = mongoose.Schema
 let ingredientSchema = new Schema({
   name: { type: String, required: true },
   group: { type: String, required: true },
-  nutrients: {
-    fat1: { type: Number },
-    fat2: { type: Number },
-    fat3: { type: Number },
-    carbs: { type: Number },
-    protein: { type: Number },
-    salt: { type: Number }
-  }
+  nutrients: {type: Object, required: true }
 })
 
 module.exports = mongoose.model("Ingredient", ingredientSchema)
