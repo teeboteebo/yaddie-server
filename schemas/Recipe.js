49a2442b9bb661879e4032fc11723d3b2b74a3f9
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-09-17 12:27:43 +0200

    Added missing keys to mongoose model

--- schemas/Recipe.js
+++ schemas/Recipe.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 let recipeSchema = new Schema({
-  name: { type: String, required: true },
+  heading: { type: String, required: true },
+  image: { type: String, required: true },
+  time: { type: Number, required: true },
+  portions: { type: Number, required: true },
   desc: { type: String, required: true },
   rating: { type: Number, required: true },
   link: { type: String, unique: true, required: true },
-  image: { type: String, required: true },
-  time: { type: Number, required: true },
   tags: [{ type: Schema.Types.ObjectId, ref: "Tag", required: true }],
   ingredients: [
     { type: Schema.Types.ObjectId, ref: "Ingredient", required: true }
-  ]
+  ],
+  instructions: { type: Array, required: true },
+  timer: { type: String }
 })
 
 module.exports = mongoose.model("Recipe", recipeSchema)
